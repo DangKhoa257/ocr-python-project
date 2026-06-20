ba5c0763722726e107bcebe52ff9df1b273d4ca0
Binh2802 <binhnp564673@ut.edu.vn>
2026-06-20 18:31:17 +0700

    Phúc Bình Update
    Updated the image loading function to include normalization and resizing.

--- image_utils.py
+++ image_utils.py
@@ -1,6 +1,15 @@
 from PIL import Image
+import numpy as np
 
-def load_image(uploaded_file):
-    image = Image.open(uploaded_file)
-    image = image.convert("RGB")
-    return image
+def load_and_normalize(uploaded_file, max_size=1024):
+    # Đọc và Resize ảnh
+    image = Image.open(uploaded_file).convert("RGB")
+    image.thumbnail((max_size, max_size), Image.Resampling.LANCZOS)
+    
+    # Chuyển sang Numpy array
+    img_array = np.array(image)
+    
+    # Chuẩn hóa giá trị pixel về khoảng [0.0, 1.0]
+    normalized_array = img_array / 255.0
+    
+    return normalized_array
