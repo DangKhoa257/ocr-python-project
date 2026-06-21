4a4d6005bfd5c6b815fefe961e3dd827b47b7501
tnhankhue-prog <tnhankhue@gmail.com>
2026-06-21 18:12:18 +0700

    Them tham so toi uu do chinh xac cho EasyOCR

--- ocr_engine.py
+++ ocr_engine.py
@@ -5,22 +5,25 @@ reader = None
 
 def get_reader():
     global reader
-
     if reader is None:
         reader = easyocr.Reader(['vi', 'en'])
-
     return reader
 
 def extract_text(image):
     image_array = np.array(image)
-
     ocr_reader = get_reader()
-    results = ocr_reader.readtext(image_array)
-
+    
+    results = ocr_reader.readtext(
+        image_array,
+        contrast_ths=0.1,
+        adjust_contrast=0.5,
+        text_threshold=0.6,
+        low_text=0.3
+    )
+    
     text_list = []
-
     for result in results:
         text = result[1]
         text_list.append(text)
-
+    
     return "\n".join(text_list)
